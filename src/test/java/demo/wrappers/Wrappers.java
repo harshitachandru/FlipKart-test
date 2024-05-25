76b1e9712cf4fd0fe6385f48e2527bed5483cdc2
Atri Biswas <atribiswas3009@gmail.com>
2024-05-25 12:18:43 +0530

    first commit

---
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -0,0 +1,22 @@
+package demo.wrappers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class Wrappers {
+    /*
+     * Write your selenium wrappers here
+     */
+}
